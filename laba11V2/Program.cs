30b7348f03eb8e00d7de2c3a6924b06c571eca06
linke <linke@DESKTOP-D9KFLQN>
2023-12-27 16:23:24 +0200

    Add project files.

---
+++ laba11V2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class DSAParametersGenerator
+{
+    static void Main()
+    {
+        using (var dsa = new DSACryptoServiceProvider())
+        {
+            // Генерація параметрів
+            var parameters = dsa.ExportParameters(true);
+
+            Console.WriteLine("Generated DSA parameters:");
+            Console.WriteLine("p: " + Convert.ToBase64String(parameters.P));
+            Console.WriteLine("q: " + Convert.ToBase64String(parameters.Q));
+            Console.WriteLine("g: " + Convert.ToBase64String(parameters.G));
+        }
+    }
+}
